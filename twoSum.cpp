b73084fc8406c777c06d978b9595168d70009e0e
liuyuxin <liuyuxin@xiaomi.com>
2017-04-13 10:03:55 +0800

    Fix compile error of twoSum.cpp.
    Signed-off-by: liuyuxin <liuyuxin@xiaomi.com>

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = number.size();
+        int n = nums.size();
         vector<int> result;
         map<int, int> index;
         for(int i=0; i<n; i++){
-            if(index.count(numbers[i])!=0)){
+            if(index.count(nums[i])!=0){
                 //if exists
-                result.push_back(index[numbers[i]] + 1);
-                result.push_back(i + 1);
+                result.push_back(index[nums[i]]);
+                result.push_back(i);
                 break;
             }
-            index[target - numbers[i]] = i;
+            index[target - nums[i]] = i;
         }
         return result;
     }
