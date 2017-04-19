7be39c80a1167301b88b859a8cc9c4563b18f973
liuyuxin <liuyuxin@xiaomi.com>
2017-04-19 22:02:41 +0800

    Add 8_stringToIntegerAtoi.cpp, don't compish.
    Signed-off-by: liuyuxin <liuyuxin@xiaomi.com>

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@ using namespace std;
 //#include "4_medianOfTwoSortedArrays.hpp"
 //#include "5_longestPalindromicSubstring.hpp"
 //#include "6_zigZagConversion.hpp"
-#include "7_reverseInteger.hpp"
+//#include "7_reverseInteger.hpp"
+#include "8_stringToIntegerAtoi.hpp"
 
 int main(int argc, char** argv)
 {
