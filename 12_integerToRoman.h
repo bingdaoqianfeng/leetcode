a28af879b2a9daf8cd76382b9749704c4766588b
liuyuxin <liuyuxin@xiaomi.com>
2017-04-23 19:30:00 +0800

    Add three.
    Signed-off-by: liuyuxin <liuyuxin@xiaomi.com>

---
+++ 12_integerToRoman.h
@@ -0,0 +1,16 @@
+/**********************************************************************************
+*
+* Given an integer, convert it to a roman numeral.
+*
+* Input is guaranteed to be within the range from 1 to 3999.
+*
+**********************************************************************************/
+class Solution {
+public:
+    string intToRoman(int num) {
+        
+    }
+	int testCase(){
+		return 0;
+	}
+};
