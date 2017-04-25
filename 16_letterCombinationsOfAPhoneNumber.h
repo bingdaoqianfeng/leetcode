be0f3097a3f3336a832c0ebcfdb527d07e560766
liuyuxin <liuyuxin@xiaomi.com>
2017-04-25 22:06:59 +0800

    to be continue...

--- 16_letterCombinationsOfAPhoneNumber.h
+++ 16_letterCombinationsOfAPhoneNumber.h
@@ -15,8 +15,20 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        
-    }
+		char telnums[10][4]={{'\0', '\0', '\0', '\0'},//0
+							{'\0', '\0', '\0', '\0'},//1
+							{'a', 'b', 'c', '\0'},//2
+							{'d', 'e', 'f', '\0'},//3
+							{'g', 'h', 'i', '\0'},//4
+							{'j', 'k', 'l', '\0'},//5
+							{'m', 'n', 'o', '\0'},//6
+							{'p', 'q', 'r', 's'},//7
+							{'t', 'u', 'v', '\0'},//8
+							{'w', 'x', 'y', 'z'},//9
+							};        
+    	vector<string> result;
+		return result;
+	}
 	int testCase(){
 		return 0;
 	}
