a8116ad1aae0d9518581614b45bd29c96cf231bb
liuyuxin <liuyuxin@xiaomi.com>
2017-04-27 15:24:29 +0800

    学习了另外一种方法

--- 16_letterCombinationsOfAPhoneNumber.h
+++ 16_letterCombinationsOfAPhoneNumber.h
@@ -14,6 +14,56 @@
 **********************************************************************************/
 class Solution {
 public:
+
+    //Methord two.这个方法有两个知识点：
+    //1. isdigit的使用
+    //2. vector可以直接赋值 result = r。
+     vector<string> letterCombinations(string digits) {
+        char  phone[10][4]={ {' ',  '\0', '\0', '\0' }, //0
+                         {'\0', '\0', '\0', '\0' }, //1
+                         {'a',  'b',  'c',  '\0' }, //2
+                         {'d',  'e',  'f',  '\0' }, //3
+                         {'g',  'h',  'i',  '\0' }, //4
+                         {'j',  'k',  'l',  '\0' }, //5
+                         {'m',  'n',  'o',  '\0' }, //6
+                         {'p',  'q',  'r',  's'  }, //7
+                         {'t',  'u',  'v',  '\0' }, //8
+                         {'w',  'x',  'y',  'z'  }  //9
+                       };
+
+        vector<string> result;
+        if (digits.size()<=0){
+            result.push_back("");
+            return result;
+        }
+        for( int i=0; i<digits.size(); i++ ) {
+            if (!isdigit(digits[i])) {
+                vector<string> r;
+                return r;
+            }
+            int d = digits[i] - '0';
+            if (result.size()<=0){
+                for( int j=0; j<4 && phone[d][j]!='\0'; j++ ){
+                    string s;
+                    s += phone[d][j];
+                    result.push_back(s);
+                }
+                continue;
+            }
+            vector<string> r;
+            for (int j=0; j<result.size(); j++){
+                for( int k=0; k<4 && phone[d][k]!='\0'; k++ ){
+                    string s = result[j] + phone[d][k];
+                    //sort(s.begin(), s.end());
+                    r.push_back(s); 
+                }
+            }
+            result = r;
+        }
+        //sort(result.begin(), result.end());
+        return result; 
+    }
+
 	
 	void combinationstr(vector<string> &result, string digits, string str){
 		char telnums[10][4]={{'\0', '\0', '\0', '\0'},//0
