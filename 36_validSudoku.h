31dac270783016d913d8cccb519ed9f0a3707a29
liuyuxin <liuyuxin@xiaomi.com>
2017-04-28 19:14:34 +0800

    add vector<<>> create function

--- 36_validSudoku.h
+++ 36_validSudoku.h
@@ -18,6 +18,33 @@ public:
         return true;
     }
     int testCase(){
+        vector< vector<char> > board(9);
+        vector<string> s(9);
+        s[0] = "53..7....";
+        s[1] = "6..195...";
+        s[2] = ".98....6.";
+        s[3] = "8...6...3";
+        s[4] = "4..8.3..1";
+        s[5] = "7...2...6";
+        s[6] = ".6....28.";
+        s[7] = "...419..5";
+        s[8] = "....8..79";
+        for(int i = 0; i<9; i++){
+            string str = s[i];
+            board[i].resize(9);
+            for(int j=0; j<9; j++){
+                printf("%c ", str[j]);
+                board[i][j] = str[j];
+            }
+        }
+
+        //print
+        for(int i = 0; i<board.size(); i++){
+            for(int j=0; j<9; j++){
+                printf("%c ", board[i][j]);;
+            }
+            printf("\n");;
+        }
         return 0;
     }
 };
