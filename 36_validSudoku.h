e089564149b890dbd545b6f41afe5a9a14cd692d
liuyuxin <liuyuxin@xiaomi.com>
2017-04-29 23:04:03 +0800

    update 36_validSudoku.h

--- 36_validSudoku.h
+++ 36_validSudoku.h
@@ -11,13 +11,47 @@
 *   Only the filled cells need to be validated.
 *
 **********************************************************************************/
-class Solution {
+/*
+三个循环，各判断行、列、小九宫格是否合法，为了节省时间，可以用bitmap来代表这个数字是否出现，bitmap可以用数组，只有0到9的数字为index，false和true为值，出现过值为true， 关于vector里面装bool类型，在<<Effective STL>> 这本书里有说明，最好不要在vector装bool类型。由于有规律，三个在不同循环判断的可以写在一个里面。
+*/
 
+class Solution {
 public:
     bool isValidSudoku(vector< vector<char> >& board) {
+		const int n = 9;
+		bool rows[n][n] = {false};		
+		bool cols[n][n]= {false};
+		bool subbox[3][3] = {false};
+		for(int i = 0; i<n; i++){
+			for(int j=0; j<n; j++){
+				char val;
+				val = board[i][j];
+				if(val != '.'){
+					int row = val - '0' - 1;
+					if(rows[i][row] == false){
+						rows[i][row] = true;
+					}
+					else{
+						printf("error: row: %d, row: %d, col: %d \n", row, i, j);
+						return false;
+					}
+				}
+				val = board[j][i];
+				if(val != '.'){
+					int col = val - '0' - 1;
+					if(cols[col][i] == false)
+						cols[col][i] = true;
+					else{
+						printf("col: %d, row: %d, col: %d \n", col, j, i);
+						return false;
+					}
+				}
+			}
+		}		
         return true;
     }
-    int testCase(){
+
+	int testCase(){
         vector< vector<char> > board(9);
         vector<string> s(9);
         s[0] = "53..7....";
@@ -33,18 +67,23 @@ public:
             string str = s[i];
             board[i].resize(9);
             for(int j=0; j<9; j++){
-                printf("%c ", str[j]);
+                //printf("%c ", str[j]);
                 board[i][j] = str[j];
             }
         }
 
-        //print
+        printboard(board);
+
+		printf("is valaid sudoku: %d \n",isValidSudoku(board));
+        return 0;
+    }
+	
+	void printboard(vector< vector<char> > board){
         for(int i = 0; i<board.size(); i++){
             for(int j=0; j<9; j++){
                 printf("%c ", board[i][j]);;
             }
             printf("\n");;
         }
-        return 0;
-    }
+	}
 };
