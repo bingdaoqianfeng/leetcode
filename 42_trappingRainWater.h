61bb72936515cca3e7d1d2672e649042f4afc53c
liuyuxin <liuyuxin@xiaomi.com>
2017-05-02 17:40:22 +0800

    add 39, 40, 41 and 42

---
+++ 42_trappingRainWater.h
@@ -0,0 +1,33 @@
+/**********************************************************************************
+*
+* Given n non-negative integers representing an elevation map where the width of each bar is 1,
+* compute how much water it is able to trap after raining.
+*
+* For example,
+* Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+*
+*     ^
+*     |
+*   3 |                       +--+
+*     |                       |  |
+*   2 |          +--+xxxxxxxxx|  +--+xx+--+
+*     |          |  |xxxxxxxxx|  |  |xx|  |
+*   1 |   +--+xxx|  +--+xxx+--+  |  +--+  +--+
+*     |   |  |xxx|  |  |xxx|  |  |  |  |  |  |
+*   0 +---+--+---+--+--+---+--+--+--+--+--+--+----->
+*       0  1   0  2  1   0  1  3  2  1  2  1
+*
+* The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case,
+* 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+*
+**********************************************************************************/
+class Solution {
+
+public:
+
+    int trap(vector<int>& height) {
+
+
+    }
+
+};
