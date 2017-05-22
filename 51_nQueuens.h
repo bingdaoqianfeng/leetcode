f81a8da92ab815ea440da700b594add300bc3160
liuyuxin <liuyuxin@xiaomi.com>
2017-05-22 22:42:06 +0800

    working on 51_nQueuens.h

--- 51_nQueuens.h
+++ 51_nQueuens.h
@@ -28,12 +28,77 @@
 class Solution {
 
 public:
+	void updateChessboard(vector< vector<int> >& chessboard, int n, int row, int col){
+		for(int i=0; i<n; i++) {
+			chessboard[row][i] = 1;
+			chessboard[i][col] = 1;	
+		}
+
+		int lefttoprow = row - 1;
+		int lefttopcol = col - 1;
+		while(lefttoprow >= 0 && lefttopcol >= 0){
+			chessboard[lefttoprow][lefttopcol] = 1;
+			lefttoprow--;
+			lefttopcol--;
+		}
+		
+		int righttoprow = row - 1;
+		int righttopcol = col + 1;
+		while(righttoprow >= 0 && righttopcol < n){
+			chessboard[righttoprow][righttopcol] = 1;
+			righttoprow--;
+			righttopcol++;
+		}
+		
+		int leftbottomrow = row + 1;
+		int leftbottomcol = col - 1;
+		while(leftbottomrow < n && leftbottomcol >= 0){
+			chessboard[leftbottomrow][leftbottomcol] = 1;
+			leftbottomrow++;
+			leftbottomcol--;
+		}
+
+		int rightbottomrow = row + 1;
+		int rightbottomcol = col + 1;
+		while(rightbottomrow < n && rightbottomcol < n){
+			chessboard[rightbottomrow][rightbottomcol] = 1;
+			rightbottomrow++;
+			rightbottomcol++;
+		}
+	}
+
     vector< vector<string> > solveNQueens(int n) {
         vector< vector<string> > result;
+
+		for(int i=0; i<n; i++){
+			vector< vector<int> > chessboard(n, vector<int>(n, 0));
+			chessboard[0][i] = 1;
+			string s(n,'.');
+			s[i] = 'Q';
+			printf("i: %d\n", i);
+			updateChessboard(chessboard, n, 0, i);
+			printVV(chessboard);	
+		}
+
         return result;
     }
     int testCase(){
+        vector< vector<string> > result;
+		int n;
+		n = 3;
+		result = solveNQueens(n);	
         return 0;
+    }
+    void printVV(vector< vector<int> >& v)
+    {
+        cout << "[ " <<endl;
+        for(int i=0; i<v.size(); i++){
+        	for(int j=0; j<v[i].size(); j++){
+            	cout<<v[i][j];
+        	}
+			cout<<" "<<endl;
+		}
+        cout << "] " << endl;
     }
     void printVectorVector(vector< vector<string> >& v)
     {
