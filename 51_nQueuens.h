f701a2aaadfef499bc0d0a6e513cc3f86517829c
liuyuxin <liuyuxin@xiaomi.com>
2017-05-23 14:47:06 +0800

    adjust the format

--- 51_nQueuens.h
+++ 51_nQueuens.h
@@ -1,103 +1,103 @@
-/********************************************************************************** 
-* 
-* The n-queens puzzle is the problem of placing n queens on an n×n chessboard 
+/**********************************************************************************
+*
+* The n-queens puzzle is the problem of placing n queens on an n×n chessboard
 * such that no two queens attack each other.
-* 
+*
 * Given an integer n, return all distinct solutions to the n-queens puzzle.
-* 
-* Each solution contains a distinct board configuration of the n-queens' placement, 
+*
+* Each solution contains a distinct board configuration of the n-queens' placement,
 * where 'Q' and '.' both indicate a queen and an empty space respectively.
-* 
+*
 * For example,
 * There exist two distinct solutions to the 4-queens puzzle:
-* 
+*
 * [
 *  [".Q..",  // Solution 1
 *   "...Q",
 *   "Q...",
 *   "..Q."],
-* 
+*
 *  ["..Q.",  // Solution 2
 *   "Q...",
 *   "...Q",
 *   ".Q.."]
 * ]
-* 
-*               
+*
+*
 **********************************************************************************/
 class Solution {
 
 public:
-	void updateChessboard(vector< vector<int> >& chessboard, int n, int row, int col){
-		for(int i=0; i<n; i++) {
-			chessboard[row][i] = 1;
-			chessboard[i][col] = 1;	
-		}
+    void updateChessboard(vector< vector<int> >& chessboard, int n, int row, int col){
+        for(int i=0; i<n; i++) {
+            chessboard[row][i] = 1;
+            chessboard[i][col] = 1;
+        }
 
-		int lefttoprow = row - 1;
-		int lefttopcol = col - 1;
-		while(lefttoprow >= 0 && lefttopcol >= 0){
-			chessboard[lefttoprow][lefttopcol] = 1;
-			lefttoprow--;
-			lefttopcol--;
-		}
-		
-		int righttoprow = row - 1;
-		int righttopcol = col + 1;
-		while(righttoprow >= 0 && righttopcol < n){
-			chessboard[righttoprow][righttopcol] = 1;
-			righttoprow--;
-			righttopcol++;
-		}
-		
-		int leftbottomrow = row + 1;
-		int leftbottomcol = col - 1;
-		while(leftbottomrow < n && leftbottomcol >= 0){
-			chessboard[leftbottomrow][leftbottomcol] = 1;
-			leftbottomrow++;
-			leftbottomcol--;
-		}
+        int lefttoprow = row - 1;
+        int lefttopcol = col - 1;
+        while(lefttoprow >= 0 && lefttopcol >= 0){
+            chessboard[lefttoprow][lefttopcol] = 1;
+            lefttoprow--;
+            lefttopcol--;
+        }
 
-		int rightbottomrow = row + 1;
-		int rightbottomcol = col + 1;
-		while(rightbottomrow < n && rightbottomcol < n){
-			chessboard[rightbottomrow][rightbottomcol] = 1;
-			rightbottomrow++;
-			rightbottomcol++;
-		}
-	}
+        int righttoprow = row - 1;
+        int righttopcol = col + 1;
+        while(righttoprow >= 0 && righttopcol < n){
+            chessboard[righttoprow][righttopcol] = 1;
+            righttoprow--;
+            righttopcol++;
+        }
+
+        int leftbottomrow = row + 1;
+        int leftbottomcol = col - 1;
+        while(leftbottomrow < n && leftbottomcol >= 0){
+            chessboard[leftbottomrow][leftbottomcol] = 1;
+            leftbottomrow++;
+            leftbottomcol--;
+        }
+
+        int rightbottomrow = row + 1;
+        int rightbottomcol = col + 1;
+        while(rightbottomrow < n && rightbottomcol < n){
+            chessboard[rightbottomrow][rightbottomcol] = 1;
+            rightbottomrow++;
+            rightbottomcol++;
+        }
+    }
 
     vector< vector<string> > solveNQueens(int n) {
         vector< vector<string> > result;
 
-		for(int i=0; i<n; i++){
-			vector< vector<int> > chessboard(n, vector<int>(n, 0));
-			chessboard[0][i] = 1;
-			string s(n,'.');
-			s[i] = 'Q';
-			printf("i: %d\n", i);
-			updateChessboard(chessboard, n, 0, i);
-			printVV(chessboard);	
-		}
+        for(int i=0; i<n; i++){
+            vector< vector<int> > chessboard(n, vector<int>(n, 0));
+            chessboard[0][i] = 1;
+            string s(n,'.');
+            s[i] = 'Q';
+            printf("i: %d\n", i);
+            updateChessboard(chessboard, n, 0, i);
+            printVV(chessboard);
+        }
 
         return result;
     }
     int testCase(){
         vector< vector<string> > result;
-		int n;
-		n = 3;
-		result = solveNQueens(n);	
+        int n;
+        n = 3;
+        result = solveNQueens(n);
         return 0;
     }
     void printVV(vector< vector<int> >& v)
     {
         cout << "[ " <<endl;
         for(int i=0; i<v.size(); i++){
-        	for(int j=0; j<v[i].size(); j++){
-            	cout<<v[i][j];
-        	}
-			cout<<" "<<endl;
-		}
+            for(int j=0; j<v[i].size(); j++){
+                cout<<v[i][j];
+            }
+            cout<<" "<<endl;
+        }
         cout << "] " << endl;
     }
     void printVectorVector(vector< vector<string> >& v)
